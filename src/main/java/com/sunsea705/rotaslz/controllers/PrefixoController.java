2878f8abb537bdd108a7d19365ec64e759a30008
Carolina Souza <sunsea.t705@gmail.com>
2025-07-10 11:05:49 -0300

    documentação swagger

--- src/main/java/com/sunsea705/rotaslz/controllers/PrefixoController.java
+++ src/main/java/com/sunsea705/rotaslz/controllers/PrefixoController.java
@@ -4,6 +4,8 @@ import com.sunsea705.rotaslz.entities.Prefixo;
 import com.sunsea705.rotaslz.models.Resposta;
 import com.sunsea705.rotaslz.requests.SalvarPrefixoRequest;
 import com.sunsea705.rotaslz.services.PrefixoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,11 +15,16 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/prefixo")
+@Tag(name = "Prefixo", description = "Operações REST a respeito da entidade Prefixo.")
 public class PrefixoController {
 
     private final PrefixoService prefixoService;
 
     @PostMapping
+    @Operation(
+        summary = "Salva/Edita informações de um Prefixo.",
+        description = "Passe um ID para editar, não passe ou passe nulo para criar um novo."
+    )
     public ResponseEntity<Resposta<Prefixo>> salvar(@RequestBody SalvarPrefixoRequest salvarPrefixoRequest) {
         return ResponseEntity.ok(
                 Resposta.<Prefixo>builder()
